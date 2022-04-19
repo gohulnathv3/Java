c5d8036e552b47d3e965b49f0e85d210771d6285
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-19 17:15:24 +0530

    - Playing with methods

--- WelcomeJava/src/io/myplayground/PlayGround.java
+++ WelcomeJava/src/io/myplayground/PlayGround.java
@@ -1,14 +1,69 @@
 package io.myplayground;
-public class PlayGround {
-
+public class PlayGround{
     public static void main(String[] args) {
-        System.out.println(calAvg(7,9,11));
+        System.out.println("Hello guys");
+        methodOne(); //calling method
+        System.out.println(methodTwo(2,5));
     }
+    public static void methodOne(){
+        System.out.println("methodOne");
+    }
+    public static int methodTwo(int num1, int num2){ //parameters adding
 
-    public static double calAvg(int a, int b, int c){
-        return (a+b+c)/3;
+        return num1+num2;
     }
 }
+
+class AnotherClass{
+    String person_name;
+    String person_gender;
+    int person_age;
+    void showPersonName(){
+        System.out.println();
+    }
+}
+
+
+
+//one way of generating pattern in java using direct method in main class.
+//public class PlayGround {
+//    public static void main(String[] args) {
+//        int i, j;
+//        for (i = 1; i <= 10; i++) {
+//            for (j = 1; j <= i; j++) {
+//                System.out.print(i);
+//            }
+//            System.out.println("\n");
+//        }
+//
+//    }
+//}
+
+
+
+//    public void String printResult(){
+//        int i,j;
+//        for(i=1;i<=10;i++){
+//            for(j=1;j<=i;j++){
+//                System.out.println("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+
+
+
+//public class PlayGround {
+//
+//    public static void main(String[] args) {
+//        System.out.println(calAvg(7,9,11));
+//    }
+//
+//    public static double calAvg(int a, int b, int c){
+//        return (a+b+c)/3;
+//    }
+//}
 
 //public class PlayGround {
 //    public static void main(String[] args) {
