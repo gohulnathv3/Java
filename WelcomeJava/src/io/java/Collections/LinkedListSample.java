8022c0997368f07e5499b9f4aef523c5c058cab5
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-25 19:41:49 +0530

    Create LinkedListSample.java
    - This is example of linked list

---
+++ WelcomeJava/src/io/java/Collections/LinkedListSample.java
@@ -0,0 +1,19 @@
+package io.java.Collections;
+
+import java.util.LinkedList;
+
+//this is about LinkedList sample
+public class LinkedListSample {
+    public static void main(String[] args) {
+        LinkedList<String> myList = new LinkedList();
+        myList.add("H");
+        myList.addFirst("A");
+        myList.addLast("End");
+//        myList.remove(); // it will remove the first element of linked list.
+
+        for(String s:myList){
+            System.out.println(s);
+        }
+
+    }
+}
