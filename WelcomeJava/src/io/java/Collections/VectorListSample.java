75c4a2978b98712c84478b991daf7b232c86f429
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-25 21:01:31 +0530

    Create VectorListSample.java

---
+++ WelcomeJava/src/io/java/Collections/VectorListSample.java
@@ -0,0 +1,17 @@
+package io.java.Collections;
+
+import java.util.Vector;
+
+public class VectorListSample {
+    public static void main(String[] args) {
+        Vector<String> myList = new Vector();
+        myList.add("a");
+        myList.add("b");
+        myList.add("c");
+        myList.addElement("Hello"); //this is the additional feature in the vector list
+
+        for(String s:myList){
+            System.out.println(s);
+        }
+    }
+}
