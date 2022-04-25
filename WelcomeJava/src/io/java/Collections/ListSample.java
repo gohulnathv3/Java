88d1b40310b6f18f495c4dbf07211517d418fc63
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-25 19:41:28 +0530

    ArrayList example

---
+++ WelcomeJava/src/io/java/Collections/ListSample.java
@@ -0,0 +1,33 @@
+package io.java.Collections;
+// this is about ArrayList sample
+import io.java.constructors.pkg1.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListSample {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList();
+        myList.add("Apple");
+        myList.add("Pinapple");
+        myList.add("Orange");
+        Collections.sort(myList);
+
+
+//        System.out.println(myList.get(0));
+//        System.out.println(myList.get(1));
+//        System.out.println(myList.get(2));
+        //print numbers using for-each loop concept
+
+        for(String fruit:myList){
+            System.out.println(fruit);
+        }
+
+//        myList.add(new Person("Alex",43,"Male");
+//        System.out.println(((Person)myList.get(1)).name);
+
+
+
+
+    }
+}
