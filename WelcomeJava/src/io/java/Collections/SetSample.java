48ec03607ad8174662720fd16da73e9a53dc776f
Gohul Nath V <gohulnath1998@gmail.com>
2022-04-26 20:06:43 +0530

    Collections
    - Added set example.

---
+++ WelcomeJava/src/io/java/Collections/SetSample.java
@@ -0,0 +1,31 @@
+package io.java.Collections;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SetSample {
+    public static void main(String[] args) {
+        HashSet<String> mySet = new HashSet<>();
+        mySet.add("Apple");
+        mySet.add("Orange");
+        mySet.add("Pine Apple");
+        mySet.add("Banana");
+        mySet.add("Apple");
+        mySet.add("Orange");
+
+        // did you see the difference here, it ignored the repeated values.
+//        for(String fruits:mySet){
+//            System.out.println(fruits);
+//        }
+        HashSet<String> mySet1 = new HashSet<>(mySet);
+        mySet1.add("Apple 1");
+        mySet1.add("Apple 1");
+        mySet1.add("Home 1");
+
+        for(String fruits:mySet1){
+            System.out.println(fruits);
+        }
+
+
+    }
+}
