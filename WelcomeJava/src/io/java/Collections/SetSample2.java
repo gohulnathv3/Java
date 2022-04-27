87836d20bd763a656e870af9c24a64a3a217a5ac
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-27 11:01:58 +0530

    Update SetSample2.java
    - LinkedHashSet example

--- WelcomeJava/src/io/java/Collections/SetSample2.java
+++ WelcomeJava/src/io/java/Collections/SetSample2.java
@@ -1,19 +1,32 @@
 package io.java.Collections;
 
+import java.io.StringBufferInputStream;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class SetSample2 {
     public static void main(String[] args) {
-        HashSet<Person> mySet = new HashSet<>();
-        mySet.add(new Person("Alex",33,"Male"));
-        mySet.add(new Person("Arun", 23, "Male"));
-        mySet.add(new Person("Alex",33,"Male"));
-        mySet.add(new Person("Mia", 43, "Female"));
+//        HashSet<Person> mySet = new HashSet<>();
+//        mySet.add(new Person("Alex",33,"Male"));
+//        mySet.add(new Person("Arun", 23, "Male"));
+//        mySet.add(new Person("Alex",33,"Male"));
+//        mySet.add(new Person("Mia", 43, "Female"));
+//
+//        for (Person list: mySet) {
+//            System.out.println(list.getName());
+//        }
 
-        for (Person list: mySet) {
-            System.out.println(list.getName());
+//        LinkedHashSet<String> mySet = new LinkedHashSet<>();
+        HashSet<String> mySet = new HashSet<>();
+        mySet.add("Orange");
+        mySet.add("Apple");
+        mySet.add("Mango");
+        mySet.add("Apple");
+
+        for(String fruits: mySet){
+            System.out.println(fruits);
         }
 
-        
+
     }
 }
