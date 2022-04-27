e555fce8e6f559f9ffa1130f45dd986d8540162f
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-27 15:43:08 +0530

    Collections - Map
    - Tried different Map functions.

--- WelcomeJava/src/io/java/Collections/MapExample.java
+++ WelcomeJava/src/io/java/Collections/MapExample.java
@@ -1,15 +1,22 @@
 package io.java.Collections;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapExample {
     public static void main(String[] args) {
-        HashMap<String, String> myMap = new HashMap<>();
+//        HashMap<String, String> myMap = new HashMap<>();
+//        Hashtable<String, String> myMap = new Hashtable<>(); //it doesn't run if null key-value is present in map.
+//        TreeMap<String, String> myMap = new TreeMap<>(); // it showed in ascending order.
+        LinkedHashMap<String, String> myMap = new LinkedHashMap<>(); // it showed in ascending order of value.
+
         myMap.put("IN", "India");
         myMap.put("US", "America");
         myMap.put("CAD", "Canada");
         myMap.put("UK", "London");
+//        myMap.put(null, "China");
+        myMap.put("CN", "China");
+//        myMap.remove("IN");
+
 
 //        System.out.println(myMap.get("IN"));
 //        to print key and values in this Map
