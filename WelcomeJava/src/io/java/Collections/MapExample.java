b6d067362950c6e4cdd08adb603e589c7cc8d369
Gohul Nath V <gohulnath1998@gmail.com>
2022-04-27 13:55:09 +0530

    Collections - Map
    - HashMap Sample 1 added

---
+++ WelcomeJava/src/io/java/Collections/MapExample.java
@@ -0,0 +1,16 @@
+package io.java.Collections;
+
+import java.util.HashMap;
+
+public class MapExample {
+    public static void main(String[] args) {
+        HashMap<String, String> myMap = new HashMap<>();
+        myMap.put("IN", "India");
+        myMap.put("US", "America");
+        myMap.put("CAD", "Canada");
+        myMap.put("UK", "London");
+
+        System.out.println(myMap.get("IN"));
+        
+    }
+}
