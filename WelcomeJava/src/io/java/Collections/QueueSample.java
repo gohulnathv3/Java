f8d8aa9f0b9d40ece1334633be87457993bfa790
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-27 18:41:32 +0530

    Collections - Queue
    - Queue file added

---
+++ WelcomeJava/src/io/java/Collections/QueueSample.java
@@ -0,0 +1,26 @@
+package io.java.Collections;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueSample {
+    public static void main(String[] args) {
+//        Queue<String> queue = new LinkedList<>();
+        Queue<String> queue = new PriorityQueue<>();
+
+        queue.add("Apple");
+        queue.add("Orange");
+        queue.add("Grapes");
+        queue.add("Guava");
+        queue.add("Watermelon");
+//        queue.poll();
+
+
+//        queue.remove();
+
+        for(String fruits: queue){
+            System.out.println(fruits);
+        }
+    }
+}
