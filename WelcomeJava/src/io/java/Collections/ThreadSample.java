4e1b9233351c220c78db1940be6a28fe470a9692
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-28 17:06:08 +0530

    Thread
    - Here we've seen sleep, wait, and run method.
    - Also seen about main and synchronized
    - Thread States

--- WelcomeJava/src/io/java/Collections/ThreadSample.java
+++ WelcomeJava/src/io/java/Collections/ThreadSample.java
@@ -10,7 +10,11 @@ public class ThreadSample implements Runnable{
             for (int i = 0; i < 5; i++) {
 //            System.out.println("Value of i is: "+i);
                 try {
-                    Thread.sleep(1000); // used to run a program with 1 sec delay
+                    System.out.println("Thread going to sleep : "+Thread.currentThread().getName());
+                    Thread.sleep(10000); // used to run a program with 1 sec delay
+                    synchronized (this){
+                        this.notifyAll();
+                    }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -19,19 +23,30 @@ public class ThreadSample implements Runnable{
         }
     }
 
-    public static void main(String[] args) {
+    public void letsWait() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName()+" Entered");
+        synchronized (this){
+            wait();
+        }
+        System.out.println(Thread.currentThread().getName()+ " Thread wakeup");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
 //        ThreadSample t = new ThreadSample();
 //        t.start();
+        System.out.println("Thread: "+Thread.currentThread().getName());
         ThreadSample obj = new ThreadSample();
         Thread t1 = new Thread(obj);
         t1.setName("A");
-        Thread t2 = new Thread(obj);
-        t2.setName("B");
-        Thread t3 = new Thread(obj);
-        t3.setName("C");
+//        Thread t2 = new Thread(obj);
+//        t2.setName("B");
+//        Thread t3 = new Thread(obj);
+//        t3.setName("C");
         t1.start();
-        t2.start();
-        t3.start();
+        obj.letsWait();
+//        t2.start();
+//        t3.start();
+
 
     }
 }
