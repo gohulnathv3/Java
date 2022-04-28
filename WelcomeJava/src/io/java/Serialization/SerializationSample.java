435e7e43227ab0089970152ae81035ed76e46ca7
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-28 17:46:40 +0530

    Serialization
    - This how we implement to generate byte stream

---
+++ WelcomeJava/src/io/java/Serialization/SerializationSample.java
@@ -0,0 +1,22 @@
+package io.java.Serialization;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerializationSample {
+    public static void main(String[] args) {
+        Vehicle polo = new Vehicle("Volkswagen", "Polo GT", 2021);
+        try {
+            FileOutputStream fileout = new FileOutputStream("polo.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileout);
+            out.writeObject(polo);
+            out.close();
+            fileout.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Serialized data is saved in polo.ser");
+
+    }
+}
