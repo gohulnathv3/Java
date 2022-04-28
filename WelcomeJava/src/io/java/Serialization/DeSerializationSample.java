5bb797175816ea3a43e015b08c41ba9745bad34f
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-04-28 17:47:08 +0530

    Deserialization
    - Here we convert byte stream to object instances.

---
+++ WelcomeJava/src/io/java/Serialization/DeSerializationSample.java
@@ -0,0 +1,22 @@
+package io.java.Serialization;
+
+import java.io.*;
+
+
+public class DeSerializationSample {
+    public static void main(String[] args) {
+        Vehicle polo = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("polo.ser");
+            ObjectInputStream in= new ObjectInputStream(fileIn);
+            polo = (Vehicle)in.readObject();
+            in.close();
+            fileIn.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        System.out.println(polo.model);
+    }
+}
