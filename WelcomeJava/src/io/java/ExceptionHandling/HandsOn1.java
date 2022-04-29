b08a965f1f76ed00af801dbf4bee549915d989d3
Gohul Nath V <gohulnath1998@gmail.com>
2022-04-29 16:57:18 +0530

    Exception Handling
    - Handson1 Practice

---
+++ WelcomeJava/src/io/java/ExceptionHandling/HandsOn1.java
@@ -0,0 +1,29 @@
+package io.java.ExceptionHandling;
+
+public class HandsOn1{
+    public static void main(String[] args) {
+        try {
+            int data = 100/0;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+        try {
+            String s = null;
+            System.out.println(s.length());
+        } catch (Exception e) {
+            System.out.println("Enter valid string and current error is "+e);
+        }
+        System.out.println("To check if program is running");
+
+        try {
+            int a[] = new int[10];
+            a[12] = 23;h
+            System.out.println(a[12]);
+        }catch(Exception e){
+            System.out.println("Please enter value between 0 and 9 and current error is "+e);
+        }
+
+    }
+}
