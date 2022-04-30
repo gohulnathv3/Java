7f77712da87318f9539782df3198ef0299671af0
Gohul Nath V <gohulnath1998@gmail.com>
2022-04-30 13:50:28 +0530

    Strings
    - HandsOn Update

---
+++ WelcomeJava/src/io/java/Strings/StringEquals.java
@@ -0,0 +1,19 @@
+package io.java.Strings;
+
+import java.util.logging.SimpleFormatter;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String s1 = "Gohul";
+        String s2 = "Gohul";
+        String s3 = new String("Gohul");
+        String s4 = "GOHUL";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+        System.out.println(s3.equals(s4));
+        System.out.println(s4.equals(s1));
+
+
+    }
+}
--- WelcomeJava/src/io/java/Strings/TestImmutableString.java
+++ WelcomeJava/src/io/java/Strings/TestImmutableString.java
@@ -3,7 +3,8 @@ package io.java.Strings;
 public class TestImmutableString {
     public static void main(String[] args) {
         String s = "Sachin";
-        s.concat("Tendulkar");
+//        s.concat("Tendulkar");
+        s = s.concat(" Tendulkar");
         System.out.println(s);
     }
 }
