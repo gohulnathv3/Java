96108df72547a87e73fc09e0003236d4731c99b0
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-02 20:38:44 +0530

    Control Statement
    - If, if else, if elif else samples are added.

---
+++ WelcomeJava/src/io/java/ControlStatement/IfSample.java
@@ -0,0 +1,15 @@
+package io.java.ControlStatement;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 51;
+        if(x+y==30){
+            System.out.println("X+Y is equal to 30");
+        } else if (x+y>60) {
+            System.out.println("X+Y is greater than 60");
+        } else {
+            System.out.println("X+Y is less than 40");
+        }
+    }
+}
