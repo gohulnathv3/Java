bee2f2bf94473f0c03980ace40c419e4109cfdad
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-02 21:33:15 +0530

    Control Statement
    - For loop program added.
    - Here we can see one basic program implementation.

---
+++ WelcomeJava/src/io/java/ControlStatement/ForSample.java
@@ -0,0 +1,14 @@
+package io.java.ControlStatement;
+
+public class ForSample {
+    public static void main(String[] args) {
+        int i = 0;
+        int sum=0;
+        for (i =0;i<10;i++){
+            sum = sum+i;
+            System.out.println("Each iteration value is: "+sum);
+            System.out.println("Each iteration value of i is: "+i);
+        }
+        System.out.println("The sum of first 10 natural numbers is "+sum);
+    }
+}
