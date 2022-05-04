14669177f393d739da3ee1f6707f4f27e5e7b51e
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-04 20:17:10 +0530

    LinkedList

--- WelcomeJava/src/io/java/Collections/HandsOnList.java
+++ WelcomeJava/src/io/java/Collections/HandsOnList.java
@@ -2,15 +2,29 @@ package io.java.Collections;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class HandsOnList {
     public static void main(String[] args) {
-        // Array List
-        ArrayList<String> list = new ArrayList<String>(); // creating Array list
-        list.add("Gohul"); // Adding objects in arraylist
+//        // Array List
+//        ArrayList<String> list = new ArrayList<String>(); // creating Array list
+//        list.add("Gohul"); // Adding objects in arraylist
+//        list.add("Akash");
+//        list.add("Ajay");
+//        list.add("Vicky");
+//        // Traversing through iterator
+//        Iterator itr = list.iterator();
+//        while (itr.hasNext()){
+//            System.out.println(itr.next());
+//        }
+
+        // Linked List
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Gohul");
         list.add("Akash");
         list.add("Ajay");
         list.add("Vicky");
+
         // Traversing through iterator
         Iterator itr = list.iterator();
         while (itr.hasNext()){
