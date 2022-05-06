d8a1a28e5c74176f4158b8fa3438c4d6cb178d61
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-06 10:45:07 +0530

    Collections - ArrayList
    - Sorted List update

--- WelcomeJava/src/io/java/Collections/Arrays/ArrayExample2.java
+++ WelcomeJava/src/io/java/Collections/Arrays/ArrayExample2.java
@@ -6,13 +6,25 @@ import java.util.LinkedList;
 
 public class ArrayExample2 {
     public static void main(String[] args) {
+
         ArrayList<String> list = new ArrayList<String>();
         list.add("Gohul");
         list.add("Vicky");
         list.add("Prakash");
         list.add("Akash");
 
-        System.out.println("Unsorted Array Elements");
+        ArrayList<Integer> intlist = new ArrayList<Integer>();
+        intlist.add(12);
+        intlist.add(211);
+        intlist.add(32);
+        intlist.add(23);
+        intlist.add(42);
+        intlist.add(193);
+
+
+
+
+        System.out.println("Unsorted Array String Elements");
 
         // Traversing  loop through for-each
         for (String names: list){
@@ -23,11 +35,28 @@ public class ArrayExample2 {
         // sorting the array
         Collections.sort(list);
 
-        System.out.println("\nSorted Array Elements");
+        System.out.println("\nSorted Array String Elements");
 
         // Traversing sorted loop through for-each
         for (String sortednames: list){
             System.out.println(sortednames);
         }
+
+        System.out.println("\nUnsorted Array Integer Elements");
+
+        // Traversing  loop through for-each
+        for (Integer number: intlist){
+            System.out.println(number);
+        }
+
+        Collections.sort(intlist);
+
+        System.out.println("\nSorted Array Integer Elements");
+        // Traversing sorted loop through for-each
+        for (Integer sortedNumbers: intlist){
+            System.out.println(sortedNumbers);
+        }
+
+
     }
 }
