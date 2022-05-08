bee91ac2545dd7d8efc1295851bd64fe946f4416
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-08 18:25:15 +0530

    Collections - ArrayList
    - Here added another java class for understanding the concept of user-defined class objects in the array list.
    - And also we've seen how to iterate those objects.

---
+++ WelcomeJava/src/io/java/Collections/Arrays/ArrayExample4.java
@@ -0,0 +1,46 @@
+package io.java.Collections.Arrays;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Student{
+     int rollNo;
+     String name;
+     int age;
+
+    public Student(){
+
+    }
+
+
+    public Student(int rollNo, String name, int age) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+    }
+}
+public class ArrayExample4 {
+    public static void main(String[] args) {
+
+        // Create user-defined class objects
+        Student s1 = new Student(1,"Gohul", 23);
+        Student s2 = new Student(2, "Akash", 24);
+        Student s3 = new Student(3, "Arun", 23);
+
+        // creating arraylist
+        ArrayList<Student> al = new ArrayList<Student>();
+        al.add(s1);
+        al.add(s2);
+        al.add(s3);
+
+        // getting iterator
+        Iterator itr = al.iterator();
+
+        // traversing elements of arraylist class objects
+        while (itr.hasNext()){
+            Student st = (Student)itr.next();
+            System.out.println(st.rollNo+" "+st.name+" "+st.age);
+        }
+
+    }
+}
