c8314feea1ea08bc2fb431969fc364b26dcd3fe1
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-09 22:57:07 +0530

    Exception Handling (try-catch)
    - Here, we're able to see how to handle the errors and which blocks are executed and not executed.

---
+++ WelcomeJava/src/io/java/ExceptionHandling/TryCatchExample1.java
@@ -0,0 +1,19 @@
+package io.java.ExceptionHandling;
+
+public class TryCatchExample1 {
+    public static void main(String[] args) {
+        try
+        {
+            int data = 100/0;
+            System.out.println("Check this block of code is executed");
+            //Handling exception
+        }catch (Exception e){
+//            Here, we can give also custom error message
+            System.out.println("Integer not divided by zero");
+//            System.out.println(e);
+        }
+
+        // To check if the rest of the code is executed.
+        System.out.println("Remaining code is executing");
+    }
+}
