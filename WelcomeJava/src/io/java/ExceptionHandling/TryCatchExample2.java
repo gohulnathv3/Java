bd989fdff88fb9c4fb46d6db03e00d357dd1cd04
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-09 22:57:52 +0530

    Exception Handling
    - Here we're seeing how to resolve the exception in catch block.

---
+++ WelcomeJava/src/io/java/ExceptionHandling/TryCatchExample2.java
@@ -0,0 +1,18 @@
+package io.java.ExceptionHandling;
+
+public class TryCatchExample2 {
+    public static void main(String[] args) {
+        int i = 50;
+        int j = 0;
+        int data;
+
+        // try-catch " RESOLVE the exception in catch block
+        try
+        {
+            data = i/j; // may throw exception
+        }catch (Exception e){ // Handling exception
+            data = i/(j+1); // Resolving the exception
+            System.out.println(data);
+        }
+    }
+}
