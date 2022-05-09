b5d9eef9af31ca1be625ef2bb76ae9e9c81acbe6
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-09 23:06:21 +0530

    Exception Handling
    - Here, we saw checked exception handing.

---
+++ WelcomeJava/src/io/java/ExceptionHandling/TryCatchExample3.java
@@ -0,0 +1,20 @@
+package io.java.ExceptionHandling;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class TryCatchExample3 {
+    public static void main(String[] args) {
+        PrintWriter pw;
+
+        try{
+//            pw = new PrintWriter("goh.txt");
+            pw = new PrintWriter("");
+            pw.println("saved");
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+        // check the program flow
+        System.out.println("File Saved Successfully");
+    }
+}
