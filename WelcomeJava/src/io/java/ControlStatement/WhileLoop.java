b1ccf2f0a9ab0d18a931c1c63422d297f9250d64
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-14 19:08:12 +0530

    While Loop

---
+++ WelcomeJava/src/io/java/ControlStatement/WhileLoop.java
@@ -0,0 +1,14 @@
+package io.java.ControlStatement;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+         // initialize expression
+        int i = 1;
+        // test expression
+        while (i<6){
+            System.out.println("Hello World");
+            // Update expression
+            i++;
+        }
+    }
+}
