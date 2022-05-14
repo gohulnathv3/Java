ae2e47fcdfd4b3d094763a2584bb4fcba6ce976f
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-14 19:14:48 +0530

    Create DoWhile.java

---
+++ WelcomeJava/src/io/java/ControlStatement/DoWhile.java
@@ -0,0 +1,16 @@
+package io.java.ControlStatement;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        // Initialize counter variable
+        int i = 0;
+        do{
+            System.out.println("Print Statement");
+            i++;
+        }
+        //Checking condition
+        //  It is being checked after
+        // minimum one iteration
+        while(i<0);
+    }
+}
