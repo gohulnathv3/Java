5466b1779e1ec5ed0f8b08c42e18a84b1add5c7b
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-14 19:18:36 +0530

    Update DoWhile.java

--- WelcomeJava/src/io/java/ControlStatement/DoWhile.java
+++ WelcomeJava/src/io/java/ControlStatement/DoWhile.java
@@ -2,15 +2,31 @@ package io.java.ControlStatement;
 
 public class DoWhile {
     public static void main(String[] args) {
+
         // Initialize counter variable
-        int i = 0;
+        int i = 1;
+
+        // Do While Loop
         do{
-            System.out.println("Print Statement");
+            // Body of do-while loop
+            System.out.println("Hello World "+i +" time");
+            // Update expression
             i++;
-        }
-        //Checking condition
-        //  It is being checked after
-        // minimum one iteration
-        while(i<0);
+
+        }while(i<6);
+
+
+
+
+//        // Initialize counter variable
+//        int i = 0;
+//        do{
+//            System.out.println("Print Statement");
+//            i++;
+//        }
+//        //Checking condition
+//        //  It is being checked after
+//        // minimum one iteration
+//        while(i<0);
     }
 }
