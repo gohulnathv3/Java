7de0dc6d8d40efa003e5fe54e89e196909df0454
Gohul Nath V <gohulnath1998@gmail.com>
2022-05-19 23:19:19 +0530

    Update VectorListSample.java

--- WelcomeJava/src/io/java/Collections/VectorListSample.java
+++ WelcomeJava/src/io/java/Collections/VectorListSample.java
@@ -1,7 +1,7 @@
 package io.java.Collections;
 
 import java.util.Vector;
-
+// Revision 1
 public class VectorListSample {
     public static void main(String[] args) {
         Vector<String> myList = new Vector();
