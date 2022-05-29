67e1840233be31f42a9de00fdcd336dfca0a186d
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-29 23:54:00 +0530

    Update VariableSample2.java

--- WelcomeJava/src/io/java/variablepkg/VariableSample2.java
+++ WelcomeJava/src/io/java/variablepkg/VariableSample2.java
@@ -2,7 +2,7 @@ package io.java.variablepkg;
 
 public class VariableSample2 {
 
-    // revision 1
+    // revision 2
     int x;
     public static void main(String[] args) {
         VariableSample2 obj = new VariableSample2();
