7a006dd36a2e990f61b1bf27d1759accd7e24573
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-05-30 23:05:08 +0530

    Update HandsOnList.java

--- WelcomeJava/src/io/java/Collections/HandsOnList.java
+++ WelcomeJava/src/io/java/Collections/HandsOnList.java
@@ -1,34 +1,44 @@
 package io.java.Collections;
 
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 // Revision 2
 public class HandsOnList {
     public static void main(String[] args) {
 //        // Array List
-//        ArrayList<String> list = new ArrayList<String>(); // creating Array list
-//        list.add("Gohul"); // Adding objects in arraylist
-//        list.add("Akash");
-//        list.add("Ajay");
-//        list.add("Vicky");
-//        // Traversing through iterator
-//        Iterator itr = list.iterator();
-//        while (itr.hasNext()){
-//            System.out.println(itr.next());
-//        }
-
-        // Linked List
-        LinkedList<String> list = new LinkedList<String>();
-        list.add("Gohul");
+        ArrayList<String> list = new ArrayList<String>(); // creating Array list
+        list.add("Gohul"); // Adding objects in arraylist
         list.add("Akash");
         list.add("Ajay");
         list.add("Vicky");
 
+        try {
+            list.get(9);
+        }catch (Exception e){
+            System.out.println("This is not executed due to some error. "+ e);
+        }
         // Traversing through iterator
         Iterator<String> itr = list.iterator();
         while (itr.hasNext()){
             System.out.println(itr.next());
         }
+
+////         Linked List
+//        LinkedList<String> list = new LinkedList<String>();
+//        list.add("Gohul");
+//        list.add("Akash");
+//        list.add("Ajay");
+//        list.add("Vicky");
+//        list.add("Vicky");
+//
+//        // Operations
+//        list.remove("Vicky");
+//
+//        // Traversing through iterator
+//        Iterator<String> itr = list.iterator();
+//        while (itr.hasNext()){
+//            System.out.println(itr.next());
+//        }
 
     }
 }
