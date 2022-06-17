7d9df04a013db63293941febae48024fe778acc8
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-17 22:03:00 +0530

    Update MapExample.java

--- WelcomeJava/src/io/java/Collections/MapExample.java
+++ WelcomeJava/src/io/java/Collections/MapExample.java
@@ -1,7 +1,7 @@
 package io.java.Collections;
 
 import java.util.*;
-//Revision 2
+//Revision 3
 public class MapExample {
     public static void main(String[] args) {
         HashMap<String, String> myMap = new HashMap<>();
