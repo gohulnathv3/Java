6cd47a0c929171168b0902c7756402d9172b96d5
Gohul Nath V <gohulnath1998@gmail.com>
2022-06-19 07:48:22 +0530

    Update ArrayExample1.java

--- WelcomeJava/src/io/java/Collections/Lists/Arrays/ArrayExample1.java
+++ WelcomeJava/src/io/java/Collections/Lists/Arrays/ArrayExample1.java
@@ -2,7 +2,7 @@ package io.java.Collections.Lists.Arrays;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-// Revision 2
+// Revision 3
 public class ArrayExample1 {
     public static void main(String[] args) {
 //        old non-generic array list
