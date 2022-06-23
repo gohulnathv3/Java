d4923299c9593fe05854e525fe7aab6ea3c33636
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-24 00:12:55 +0530

    Update PlayGround.java

--- WelcomeJava/src/io/myplayground/PlayGround.java
+++ WelcomeJava/src/io/myplayground/PlayGround.java
@@ -1,35 +1,18 @@
 package io.myplayground;
 // Revision 1
-import org.w3c.dom.css.Rect;
-
-import java.util.Locale;
 
 public class PlayGround{
     public static void main(String[] args) {
-
-        Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle();
-        r1.insert(11,3);
-        r1.insert(3,15);
-        r1.calculateArea();
-        r2.calculateArea();
-
+        System.out.println("{\r\n" + " \"message\":\"Event Published successfully\"\r\n" + "}");
 
     }
+
 }
-class Rectangle{
-    int length;
-    int width;
-    void insert(int l, int w){
-        length = l;
-        width = w;
+//
 
-    }
-    void calculateArea(){
-        System.out.println(length*width);
 
-    }
-}
+
+
 
 
 //public class PlayGround {
