86fa309ca4f4f585a994d95a2ff184dc52cdcb7d
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-26 21:43:13 +0530

    - Playing with methods

---
+++ WelcomeJava/src/io/java/Collections/HashMap.java
@@ -0,0 +1,11 @@
+package io.java.Collections;
+
+
+// it is generally used to store the key value pairs
+//it has one null key and multiple null values
+public class HashMap {
+    public static void main(String[] args) {
+
+    }
+
+}
