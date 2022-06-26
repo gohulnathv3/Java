a7edbfc5c346e6c53bd127532e4de421a074045e
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-06-26 21:52:58 +0530

    - Playing with methods

---
+++ WelcomeJava/src/io/java/Collections/Lists/HashMap.java
@@ -0,0 +1,16 @@
+package io.java.Collections.Lists;
+import java.util.*;
+public class HashMap {
+    public static void main(String[] args) {
+        java.util.HashMap<Integer, String> myMap = new java.util.HashMap<>();
+        myMap.put(1, "Gohul");
+        myMap.put(2, "Ajay");
+        myMap.put(3, "Arun");
+
+        for(Map.Entry m: myMap.entrySet()){
+            System.out.println(m.getKey()+ " "+ m.getValue());
+        }
+
+
+    }
+}
