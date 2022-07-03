96313df8ba262c54f4206aeee58f5d55d2e1e946
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-03 21:17:50 +0530

    Instant variable only declared in inside the class but outside the method. It cannot be declared as static. Because, it's value is instance specific and is not shared among instances.

---
+++ WelcomeJava/src/io/java/Variable/VariableSample.java
@@ -0,0 +1,11 @@
+package io.java.Variable;
+
+public class VariableSample{
+    public static void main(String[] args) {
+        int instance = 200;
+        System.out.println("Instance variable is only declared in main : "+instance);
+    }
+}
+class Addition{
+
+}
