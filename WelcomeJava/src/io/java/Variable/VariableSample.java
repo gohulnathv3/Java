ac335014bbe04db4534fd7f3a3bc4bc35c3ed108
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-03 22:35:25 +0530

    Update VariableSample.java

--- WelcomeJava/src/io/java/Variable/VariableSample.java
+++ WelcomeJava/src/io/java/Variable/VariableSample.java
@@ -1,9 +1,16 @@
 package io.java.Variable;
 
 public class VariableSample{
+    static final int m = 100; // static variable
     public static void main(String[] args) {
-        int instance = 200;
-        System.out.println("Instance variable is only declared in main : "+instance);
+        int data = 200; // Instance Variable
+        System.out.println(m);
+        System.out.println("Instance variable is : "+data);
+    }
+
+    void method(){
+        // Local variable cannot be static
+        int n = 9; // local variable, it is used to declare the variable within method onl
     }
 }
 class Addition{
