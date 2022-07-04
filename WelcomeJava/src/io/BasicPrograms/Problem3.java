1a8b6d361cc2d594805e454ed1a629ff3bd845d4
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-04 23:25:57 +0530

    - For getting numbers from user.

---
+++ WelcomeJava/src/io/BasicPrograms/Problem3.java
@@ -0,0 +1,17 @@
+package io.BasicPrograms;
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        // declare the required data type and variable to store the data
+        int num;
+        // To deliver the message to user to do
+        System.out.println("Enter the number: ");
+        // For scanning purpose, we created a scanner object.
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt(); // read user input
+
+        System.out.println("Entered number is: "+num); // user output
+    }
+}
