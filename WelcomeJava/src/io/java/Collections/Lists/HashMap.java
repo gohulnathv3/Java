c6c48a918b16975f203ee7387d3d85dcd9f4e764
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-05 23:48:22 +0530

    Update HashMap.java

--- WelcomeJava/src/io/java/Collections/Lists/HashMap.java
+++ WelcomeJava/src/io/java/Collections/Lists/HashMap.java
@@ -1,5 +1,6 @@
 package io.java.Collections.Lists;
 import java.util.*;
+// revision 1
 public class HashMap {
     public static void main(String[] args) {
         java.util.HashMap<Integer, String> myMap = new java.util.HashMap<>();
