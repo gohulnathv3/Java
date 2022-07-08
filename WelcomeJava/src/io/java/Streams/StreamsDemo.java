f815f20589c2f4303f8fa43e5e28bd254f0dd134
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-08 23:03:16 +0530

    Update StreamsDemo.java

--- WelcomeJava/src/io/java/Streams/StreamsDemo.java
+++ WelcomeJava/src/io/java/Streams/StreamsDemo.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 public class StreamsDemo {
     public static void main(String[] args) {
+
+       // Filtering
+        String message ="{1,2,3,4,5}";
+
         List<Integer> number = Arrays.asList(2,3,4,5);
         List<Integer> square = number.stream().map(x->x*x).collect(Collectors.toList());
-
         System.out.println(square);
+
+        // Sorting
+        List<String>  names = Arrays.asList("Gohul", "Arjun", "Arun");
+        List<String> sortedNames = names.stream().filter(s->s.startsWith("A")).collect(Collectors.toList());
+        System.out.println(sortedNames);
     }
 }
