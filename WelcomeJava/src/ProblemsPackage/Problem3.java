f2ac63ba378c3066166e30192bbf2e2a6f7a3eb5
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-09 23:44:08 +0530

    - Problem3, added subtraction method for performing subtractions.

--- WelcomeJava/src/ProblemsPackage/Problem3.java
+++ WelcomeJava/src/ProblemsPackage/Problem3.java
@@ -10,6 +10,16 @@ public class Problem3 {
         System.out.println(a+b);
     }
 
+    // To create a method for subtraction
+    public void subtraction(int a, int b){
+        if(a>b){
+            System.out.println(a-b);
+        }
+        else{
+            System.out.println(b-a);
+        }
+    }
+
     //
 
 }
